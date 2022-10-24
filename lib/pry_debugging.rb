2fc7b4e902ea374742850ac4bf63a26693005a3d
kimuit117 <kemoikiuita@gmail.com>
2022-10-24 12:43:56 +0300

    fixing bugs

--- lib/pry_debugging.rb
+++ lib/pry_debugging.rb
@@ -1,4 +1,31 @@
+require 'pry'
+
+
 def plus_two(num)
-	num + 2
-	num
+  return num + 2
+    binding.pry
+    num
 end
+
+
+# def to_do (day)
+#     if day == "Monday"
+# 		"pickup trash"
+# 	elif day == "friday"
+
+# 		"weekend loading"
+# 	else 
+# 		"clean the house"
+# 	end
+# end
+
+# def do_dishes dishes
+# unless  dishes == "clean"
+#       "run the dish washer"
+# else 
+# 	"don't run the dish washer"
+# 	end
+
+# end
+
+# binding.pry
